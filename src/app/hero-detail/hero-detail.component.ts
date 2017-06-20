6ec6f0d685bb7ec4515d6fc7b2c9c1e5acaebcf8
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-06-20 16:02:19 -0300

    4: Multiple Components

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from "app/hero";
+
+@Component({
+  selector: 'hero-detail',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+export class HeroDetailComponent {
+  
+  @Input() hero: Hero;
+
+}
