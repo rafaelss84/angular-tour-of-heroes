299c1c41ec1e8b6b8190ba4e2cc18c904fab45c1
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-06-20 16:56:53 -0300

    5: Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from "app/hero";
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },  
-  { id: 12, name: 'Narco' },  
-  { id: 13, name: 'Bombasto' },  
-  { id: 14, name: 'Celeritas' },  
-  { id: 15, name: 'Magneta' },  
-  { id: 16, name: 'RubberMan' },  
-  { id: 17, name: 'Dynama' },  
-  { id: 18, name: 'Dr IQ' },  
-  { id: 19, name: 'Magma' },  
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from "app/hero.service";
 
 @Component({
   selector: 'app-root',
@@ -76,14 +64,25 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
